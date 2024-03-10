de0f47411192a4532b91d9cd1b82fa6b4fba3eb0
sourabh punase <sourabhpunase19@gmail.com>
2024-03-10 12:29:47 +0530

    add pre install

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import authrouter from './routes/auth.route.js'
 import userRouter from './routes/user.route.js'
 import listingRouter from './routes/listing.route.js'
+import path from 'path'
 dotenv.config();
 
 
@@ -17,6 +18,8 @@ mongoose.connect("mongodb+srv://sourabhpunase19:Sourabh1205@cluster0.qjnjjxe.mon
 .catch((err)=>{
     console.log(err)
 })
+const __dirname=path.resolve();
+
 const app=express();
 app.use(express.json());
 
@@ -24,6 +27,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.listen(3000,()=>{
     console.log('server is running')
+})
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
 //router for auth,listing,and user
